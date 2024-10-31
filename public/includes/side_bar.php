c1bf502af74724cdc83afaef603e48293a642f74
Jairo Santos <107869809+JairoTSantos@users.noreply.github.com>
2024-10-31 16:15:44 -0300

    v1

--- public/includes/side_bar.php
+++ public/includes/side_bar.php
@@ -1,11 +1,9 @@
 <div class="border-end bg-white" id="sidebar-wrapper">
     <div class="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
     <div class="list-group list-group-flush">
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
+        <p style="margin-left: 10px; margin-top:20px;font-weight: bolder;" class="text-muted"><i class="fas fa-bars"></i> Gestão de pessoas</p>
+        <a class="list-group-item list-group-item-action list-group-item-light px-4" href="pessoas.php"><i class="fa-solid fa-users"></i> Pessoas</a>
+        <a class="list-group-item list-group-item-action list-group-item-light px-4" href="cargos.php"><i class="fa-solid fa-briefcase"></i> Cargos</a>
+        <a class="list-group-item list-group-item-action list-group-item-light px-4" href="familias.php"><i class="fa-solid fa-people-roof"></i> Famílias</a>
     </div>
 </div>
